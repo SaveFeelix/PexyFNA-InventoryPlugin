baf6272711426cba9a8f7ff20dc3c470c4cad212
SaveFeelix <felixhake@web.de>
2023-11-05 12:28:13 +0100

    Remove line format

--- src/main/java/com/saveapis/inventoryplugin/InventoryPlugin.java
+++ src/main/java/com/saveapis/inventoryplugin/InventoryPlugin.java
@@ -9,39 +9,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InventoryPlugin extends JavaPlugin {
 
-    /**
-     * The prefix for all messages sent by the plugin.
-     */
-    public static final TextComponent PREFIX =
-            Component.text("[Inventory] ").color(NamedTextColor.BLUE);
+    public static final TextComponent PREFIX = Component.text("[Inventory] ").color(NamedTextColor.BLUE);
 
-    /**
-     * The console
-     */
-    public static final ConsoleCommandSender CONSOLE =
-            Bukkit.getConsoleSender();
+    public static final ConsoleCommandSender CONSOLE = Bukkit.getConsoleSender();
 
     @Override
     public void onLoad() {
-        TextComponent message =
-                PREFIX.append(Component.text("Plugin loaded!")
-                        .color(NamedTextColor.GOLD));
+        TextComponent message = PREFIX.append(Component.text("Plugin loaded!").color(NamedTextColor.GOLD));
         CONSOLE.sendMessage(message);
     }
 
     @Override
     public void onEnable() {
-        TextComponent message =
-                PREFIX.append(Component.text("Plugin enabled!")
-                        .color(NamedTextColor.GREEN));
+        TextComponent message = PREFIX.append(Component.text("Plugin enabled!").color(NamedTextColor.GREEN));
         CONSOLE.sendMessage(message);
     }
 
     @Override
     public void onDisable() {
-        TextComponent message =
-                PREFIX.append(Component.text("Plugin disabled!")
-                        .color(NamedTextColor.RED));
+        TextComponent message = PREFIX.append(Component.text("Plugin disabled!").color(NamedTextColor.RED));
         CONSOLE.sendMessage(message);
     }
 }
