bb50472ad92ad17ad21d53dc6248ac76c2e30e10
SaveFeelix <73217301+SaveFeelix@users.noreply.github.com>
2023-11-05 12:48:38 +0100

    InventoryPlugin v1.0.0 (#2)
    * Add .gitignore
    
    * Fill missing parts
    
    * Fix lint error
    
    * Add build job
    
    * Create maven project
    
    * Create VERSION
    
    * Enable caching
    
    * Add .iml files to ignore list
    
    * Change maven properties
    
    * Add Paper dependencies
    
    * Create base plugin structure
    
    * Try fix lint-error
    
    * Try fix lint-error
    
    * Try fix lint-error
    
    * Remove lint job
    
    * Remove package-info.java
    
    * Set documented Paper-Repository
    
    * Bump actions/setup-java from 2 to 3
    
    Bumps [actions/setup-java](https://github.com/actions/setup-java) from 2 to 3.
    - [Release notes](https://github.com/actions/setup-java/releases)
    - [Commits](https://github.com/actions/setup-java/compare/v2...v3)
    
    ---
    updated-dependencies:
    - dependency-name: actions/setup-java
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Remove line format
    
    * Create CD workflow
    
    * Create CHANGELOG.md
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/main/java/com/saveapis/inventoryplugin/InventoryPlugin.java
@@ -0,0 +1,33 @@
+package com.saveapis.inventoryplugin;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InventoryPlugin extends JavaPlugin {
+
+    public static final TextComponent PREFIX = Component.text("[Inventory] ").color(NamedTextColor.BLUE);
+
+    public static final ConsoleCommandSender CONSOLE = Bukkit.getConsoleSender();
+
+    @Override
+    public void onLoad() {
+        TextComponent message = PREFIX.append(Component.text("Plugin loaded!").color(NamedTextColor.GOLD));
+        CONSOLE.sendMessage(message);
+    }
+
+    @Override
+    public void onEnable() {
+        TextComponent message = PREFIX.append(Component.text("Plugin enabled!").color(NamedTextColor.GREEN));
+        CONSOLE.sendMessage(message);
+    }
+
+    @Override
+    public void onDisable() {
+        TextComponent message = PREFIX.append(Component.text("Plugin disabled!").color(NamedTextColor.RED));
+        CONSOLE.sendMessage(message);
+    }
+}
