16349c39dd3439a16ef12f289a7d6b446f480910
SaveFeelix <felixhake@web.de>
2023-11-05 15:56:02 +0100

    Create config models

---
+++ src/main/java/com/saveapis/inventoryplugin/models/EnchantmentInformation.java
@@ -0,0 +1,22 @@
+package com.saveapis.inventoryplugin.models;
+
+public class EnchantmentInformation {
+    private String enchantmentKey;
+    private int level;
+
+    public String getEnchantmentKey() {
+        return enchantmentKey;
+    }
+
+    public void setEnchantmentKey(String enchantmentKey) {
+        this.enchantmentKey = enchantmentKey;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
