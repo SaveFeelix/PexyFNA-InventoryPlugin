dd50521fe1d7f8406160c362014ca63b6f1deac1
SaveFeelix <felixhake@web.de>
2023-11-05 20:52:07 +0100

    Format code

--- src/main/java/com/saveapis/inventoryplugin/events/InventoryEvents.java
+++ src/main/java/com/saveapis/inventoryplugin/events/InventoryEvents.java
@@ -5,37 +5,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.InventoryInteractEvent;
 
 public class InventoryEvents implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onInventoryClose(InventoryCloseEvent event) {
-        if (event.getPlayer() instanceof Player player) {
-            if (event.getInventory() == InventoryManager.getInventory()) {
-                InventoryManager.saveInventory();
-                InventoryManager.updatePlayers(player);
-            }
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getWhoClicked() instanceof Player player) {
-            if (event.getInventory() == InventoryManager.getInventory()) {
-                InventoryManager.saveInventory();
-                InventoryManager.updatePlayers(player);
-            }
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onInventoryDrag(InventoryDragEvent event) {
-        if (event.getWhoClicked() instanceof Player player) {
-            if (event.getInventory() == InventoryManager.getInventory()) {
+        if (event.getPlayer() instanceof Player player)
+            if (event.getInventory() == InventoryManager.getInventory())
                 InventoryManager.saveInventory();
     }
 }
